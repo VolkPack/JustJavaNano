c53380661be1c9112b65024443c4dabf10ea5244
VolkPack <nikitakarayev@gmail.com>
2016-07-30 19:45:11 -0700

    Updated Layout, additional Fields and functionality added

--- app/src/main/java/com/example/nick/justjava/MainActivity.java
+++ app/src/main/java/com/example/nick/justjava/MainActivity.java
@@ -38,18 +38,21 @@ public class MainActivity extends ActionBarActivity {
         total = 0;
     }
 
+    public void resetThank()
+    {
+        TextView thanks = (TextView) findViewById(R.id.thanks);
+        thanks.setText("");
+    }
+
     public void subtractDrink(View view)
     {
         //Check if amount of drunks is zero
         //Pops Up toast Saying cannot be less then 0
+        resetThank();
+        resetTotal();
         if (amount == 0)
         {
-            Context context = getApplicationContext();
-            String toasts = getString(R.string.toast);
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, toasts, duration);
-            toast.show();
+            noDrinks();
         }
         else
         {
@@ -61,17 +64,44 @@ public class MainActivity extends ActionBarActivity {
 
     public void addDrink(View view)
     {
-
+        resetThank();
+        resetTotal();
         amount = amount + 1;
         setDrinks();
 
     }
 
+    public void noDrinks()
+    {
+        Context context = getApplicationContext();
+        String toasts = getString(R.string.toast);
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, toasts, duration);
+        toast.show();
+    }
+
     public void calculateTotal(View view)
     {
         float price = Float.parseFloat(getString(R.string.price_of_latte));
-        total = (total + (amount*price));
-        setTotal();
+        if(amount != 0)
+        {
+            total = (total + (amount * price));
+            setTotal();
+            TextView thanks = (TextView) findViewById(R.id.thanks);
+            thanks.setText(getString(R.string.thanks));
+        }
+        else
+        {
+            noDrinks();
+        }
+    }
+
+    public void resetTotal()
+    {
+        total = 0;
+        TextView total_view = (TextView) findViewById(R.id.total_due);
+        total_view.setText(Float.toString(total));
     }
 
 
